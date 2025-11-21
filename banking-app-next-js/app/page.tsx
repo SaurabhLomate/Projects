d1928d26ab9eaa99c250c4a27b784e370aae97ec
Lomate Saurabh <lomatesaurabh353@gmail.com>
2025-11-21 16:06:27 +0530

    setup project

---
+++ banking-app-next-js/app/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="input-class">
+      <button className="form-btn">Click</button>
+    </div>
+  );
+};
+
+export default App;
